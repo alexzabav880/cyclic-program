e4626a8013c0cc05fae35e6b4bca6b5490907041
alexz <alexz@4LEX7O8>
2024-03-01 14:19:40 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication5/ConsoleApplication5.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    while (true)
+    {
+
+        std::cout << "Иди нахуй";
+        
+
+
+        //std::cout << i << " * " << i << " = " << i * i << std::endl;
+        //i++;
+
+
+    }
+}
